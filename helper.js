08fbe0fe9693a58ca1f0d9374b55c7a81434521f
Guadalupe Brenes <brenesmesa@gmail.com>
2020-08-18 09:44:15 -0500

    Cuadritos se generan random y se acomodan en el tablero

--- helper.js
+++ helper.js
@@ -6,33 +6,36 @@
 function clearCanvas() {
     ctx.clearRect(0, 0, $canvas.width, $canvas.height)
 } */
+startBtn.addEventListener("click", iniciar)
 
 function iniciar() {
     img = new Image();
-    img.addEventListener(`load`, onImage, false)
+    img.addEventListener(`load`, dividirCuadritos)
     let random = Math.floor(Math.random() * imagenesColors.length)
     img.src = imagenesColors[random]
+    //creo que el problema es el random aquí
 }
 
-function dividirCuadritos(e) {
-    cuadritoswidth = Math.floor($canvas.width / nivel)
-    cuadritosheight = Math.floor($canvas.height / nivel)
-        //set canvas y init puzle cambiaran
-        //setCanvas();
-    initPuzzle();
+function dividirCuadritos() {
+    cuadritoswidth = $canvas.width / nivel
+    cuadritosheight = $canvas.height / nivel
+    //set canvas y init puzle cambiaran
+    //setCanvas();
+    empezarQiyi();
 }
 
 function empezarQiyi() {
-    ctx.drawImage(img, 0, 0, $canvas.width, $canvas.height, 0, 0, $canvas.width, $canvas.height)
-        //esta función (buildpieces) puede cambiar
+    // ctx.drawImage(img, 0, 0, $canvas.width, $canvas.height)
+    //esta función (buildpieces) puede cambiar
     generarPiezas()
 }
 
 function generarPiezas() {
-    let cuadro = {};
+    let cuadro;
     let xPos = 0;
     let yPos = 0;
     for (let i = 0; i < nivel * nivel; i++) {
+        cuadro = {};
         cuadro.sx = xPos;
         cuadro.sy = yPos;
         cuadritos.push(cuadro);
@@ -42,47 +45,68 @@ function generarPiezas() {
             yPos += cuadritosheight;
         }
     }
+    cuadritos.pop()
     //crear las piezas al dar clic en algun boton
-    document.onmousedown = shuffleCuadritos(cuadritos);
-
-
+    // document.onmousedown = 
+    shuffleCuadritos(cuadritos);
 }
 
 
 function shuffleCuadritos() {
 
     cuadritos = shuffleArray(cuadritos)
-    $canvas.clearReact(0, 0, cuadritoswidth, cuadritosheight);
+    // $canvas.clearReact(0, 0, $canvas.width, $canvas.height);
     let cuadro;
-    let xPos = 0;
+    let xPos = 0
     let yPos = 0;
-
     for (let i = 0; i < cuadritos.length; i++) {
         cuadro = cuadritos[i];
-        piece.xPos = xPos;
-        piece.yPos = yPos;
-        ctx.drawImage(img, piece.sx, piece.sy, cuadritoswidth, cuadritosheight, xPos, yPos, cuadritoswidth, cuadritosheight);
+        cuadro.xPos = xPos;
+        cuadro.yPos = yPos
+        ctx.drawImage(img, cuadro.sx, cuadro.sy, cuadritoswidth, cuadritosheight, xPos, yPos, cuadritoswidth, cuadritosheight);
         xPos += cuadritoswidth;
-        if (xPos >= cuadritoswidth) {
+        if (xPos >= $canvas.width) {
             xPos = 0;
-            yPos += cuadritosheight
+            yPos += cuadritoswidth
         }
     }
 
     //posible cambio de accion
-    document.onmousedown = onPuzzleClick;
-
-
-
+    // document.onmousedown = onPuzzleClick;
 }
 
 function shuffleArray(array) {
-
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
-
     }
     return array
+}
 
+$canvas.addEventListener('mousedown', e => {
+    mouseDown = true;
+    mouseX = e.pageX - $canvas.offsetLeft; 
+    mouseY = e.pageY - $canvas.offsetTop;
+})
+
+function mover() {
+    espacioX = ($canvas.width / nivel) * (nivel - 1)
+    espacioY = ($canvas.height / nivel) * (nivel - 1)
+    for (i = 0; i < cuadritos.length; i++) {
+        if (mouseDown) {
+            if (cuadritos[i].sx < mouseX && (cuadritos[i].sx + cuadritoswidth) > mouseX && cuadritos[i].sy < mouseY && (cuadritos[i].sy + cuadritosheight) > mouseY) {
+                if (cuadritos[i].sx == espacioX && (cuadritos[i].sy == espacioY - cuadritosheight || cuadritos[i].sy == espacioY + cuadritosheight) || cuadritos[i].sy == espacioY && (cuadritos[i].sx == espacioX - cuadritoswidth || cuadritos[i].sx == espacioX + cuadritoswidth)) {
+                        ctx.clearRect(cuadritos[i].sx, cuadritos[i].sy, cuadritoswidth, cuadritosheight)
+                        nuevoEspacioX = cuadritos[i].sx
+                        nuevoEspacioY = cuadritos[i].sy
+                        cuadritos[i].sx = espacioX
+                        cuadritos[i].sy = espacioY
+                        espacioX = nuevoEspacioX
+                        espacioY = nuevoEspacioY
+                }
+            }
+        ctx.drawImage(img, cuadritos[i].sx, cuadritos[i].sy, cuadritoswidth, cuadritosheight, 50, 50, cuadritoswidth, cuadritosheight)
+
+        }
+    }
 }
--- variables.js
+++ variables.js
@@ -3,9 +3,11 @@ let img;
 let cuadritos = [];
 let cuadritosheight;
 let cuadritoswidth;
+let espacioX
+let espacioY
 let mouse = { x: 0, y: 0 };
-let imagenesColors = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+let imagenesColors = ["./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg"];
 
+// El número que es divisible entre 3, 4 y 5 es 180, o sea que puede ser de: 360 ó 720
 
-
-intervalId = setInterval(update, 1000 / 60)
+// intervalId = setInterval(update, 1000 / 60)
